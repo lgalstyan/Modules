47d3d4f434a644ed187baaa1b241292ece6d4123
lgalstyan <larisagalstya@gmail.com>
2023-04-09 10:21:52 +0400

    geters done

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,8 +1,26 @@
 #include "Account.hpp"
 
+int    Account::getNbAccounts(void)
+{
+    return (_nbAccounts);
+}
+int    Account::getTotalAmount(void)
+{
+    return (_totalAmount);
+}
+int    Account::getNbDeposits(void)
+{
+    return (_totalNbDeposits);
+}
+int    Account::getNbWithdrawals(void)
+{
+    return (_totalNbWithdrawals);
+}
+// void    Account::	displayAccountsInfos( void );
+
 void	Account::makeDeposit(int deposit)
 {
-    
+
 }
 
 bool	Account::makeWithdrawal(int withdrawal)

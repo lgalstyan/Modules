9f4367dfdf706fa92af62124a3e1fb9a386ba103
lgalstyan <larisagalstya@gmail.com>
2023-04-09 10:37:11 +0400

    display infos

--- ex02/Account.cpp
+++ ex02/Account.cpp
@@ -1,4 +1,5 @@
 #include "Account.hpp"
+#include <iostream>
 
 int    Account::getNbAccounts(void)
 {
@@ -16,7 +17,16 @@ int    Account::getNbWithdrawals(void)
 {
     return (_totalNbWithdrawals);
 }
-// void    Account::	displayAccountsInfos( void );
+
+//[19920104_091532] accounts:8;total:12442;deposits:8;withdrawals:6
+
+void    Account::displayAccountsInfos(void)
+{
+    std::cout << "account:" << getNbAccounts() << ";";
+    std::cout << "total:" << getTotalAmount() << ";";
+    std::cout << "deposit:" << getNbDeposits() << ";";
+    std::cout << "withdrawal:" << getNbWithdrawals() << "\n";
+}
 
 void	Account::makeDeposit(int deposit)
 {
@@ -33,7 +43,11 @@ int		Account::checkAmount(void) const
 
 }
 
+// [19920104_091532] index:6;amount:763;deposits:1;withdrawals:0
 void	Account::displayStatus(void) const
 {
-
+    std::cout << "index:" << this->_accountIndex << ";";
+    std::cout << "amount:" << this->_amount << ";";
+    std::cout << "deposits:" << this->_nbDeposits << ";";
+    std::cout << "withdrawals:" << this->_nbWithdrawals << "\n";
 }

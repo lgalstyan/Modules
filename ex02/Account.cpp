3f40319ef7dbad392b382e54ea52da56539cb602
lgalstyan <larisagalstya@gmail.com>
2023-04-09 10:14:58 +0400

    Add Makefile

---
+++ ex02/Account.cpp
@@ -0,0 +1,21 @@
+#include "Account.hpp"
+
+void	Account::makeDeposit(int deposit)
+{
+    
+}
+
+bool	Account::makeWithdrawal(int withdrawal)
+{
+
+}
+
+int		Account::checkAmount(void) const
+{
+
+}
+
+void	Account::displayStatus(void) const
+{
+
+}

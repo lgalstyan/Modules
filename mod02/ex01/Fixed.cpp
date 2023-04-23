9a75a452dbe48327f68d5485854baa44ec8b071f
lgalstyan <larisagalstya@gmail.com>
2023-04-23 21:11:10 +0400

    Don't understand how to implement floating point numbers

--- mod02/ex01/Fixed.cpp
+++ mod02/ex01/Fixed.cpp
@@ -1,10 +1,25 @@
 #include "Fixed.hpp"
 
+static const int _fract_bits = 8;
+
 Fixed::Fixed()
 {
     std::cout << "Default constructor called\n";
     _fix_p = 0;
 }
+
+Fixed::Fixed(const int nf)
+{
+    std::cout << "Int constructor called\n";
+
+}
+
+Fixed::Fixed(const float nf)
+{
+    std::cout << "Float constructor called\n";
+
+}
+
 Fixed::Fixed(const Fixed &other)
 {
     std::cout << "Copy constructor called\n";
@@ -33,4 +48,10 @@ void Fixed::setRawBits(int const raw)
 {
     std::cout << "setRawBits member function called\n";
     _fix_p = raw;
+}
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fix)
+{
+    os << "aa";
+    return os;
 }
--- mod02/ex01/Fixed.hpp
+++ mod02/ex01/Fixed.hpp
@@ -7,15 +7,21 @@ class Fixed
 {
     public:
         Fixed();
+        Fixed(const int nf);
+        Fixed(const float nf);
         Fixed(const Fixed &other);
         Fixed& operator= (const Fixed& rhs); //right hand side
         ~Fixed();
         int getRawBits(void) const;
         void setRawBits(int const raw);
-    
+        int toInt( void ) const;
+        float toFloat( void ) const;
+
     private:
         int _fix_p;
-	    static const int _fract_bits = 8;
+	    static const int _fract_bits;
 };
+
+std::ostream& operator<<(std::ostream& os, const Fixed& fix);
 
 #endif
